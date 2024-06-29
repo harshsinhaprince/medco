39f06fb264e164b75facd737897358e88cf285f1
aishwarysingh7754 <66073060+aishwarysingh7754@users.noreply.github.com>
2024-06-29 16:40:17 +0530

    Created userService Version-1.1

--- userService/userService/src/main/java/com/medicine/userService/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medicine.userService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.medicine.userService.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-}
