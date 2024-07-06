713f70e560322d6f8f3e899bb64c49183571e9b7
aishwarysingh7754 <66073060+aishwarysingh7754@users.noreply.github.com>
2024-07-07 00:06:04 +0530

    Solved the error and tested the application

--- userService/src/main/java/com/medicine/userService/controller/UserController.java
+++ userService/src/main/java/com/medicine/userService/controller/UserController.java
@@ -1,24 +1,19 @@
 package com.medicine.userService.controller;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.medicine.userService.model.Users;
 import com.medicine.userService.service.UserService;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
-
+    @GetMapping("/private")
+    public ResponseEntity<String> privateEndpoint() {
+        return ResponseEntity.ok("You accessed a private endpoint!");
+    }
 }
